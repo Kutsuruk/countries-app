6de9ae82bb9ff14f5bf9a0d0f941ad443f37288d
Vitalii Kutsuruk <kutsurukvet@gmail.com>
2022-08-01 23:16:15 +0300

    Createed API links for country. Get country from API and styles every card component of country.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Controls from "./components/Controls";
+import axios from "axios";
+import {ALL_COUNTRIES} from "./config";
+import List from "./components/List";
+import Card from "./components/Card";
 
 function App() {
+    const [countries, setCountries] = useState([])
+
+    useEffect(() => {
+        axios.get(ALL_COUNTRIES).then(
+            ({data}) => setCountries(data)
+        )
+    }, [])
+
   return (
     <>
         <Header />
         <Main>
             <Controls />
+            <List>
+                {
+                    countries.map(country => {
+                        const countryInfo = {
+                            img: country.flags.svg,
+                            name: country.name,
+                            info: [
+                                {
+                                title: 'Population',
+                                description: country.population.toLocaleString()
+                                },
+                                {
+                                    title: 'Region',
+                                    description: country.region.toLocaleString()
+                                },
+                                {
+                                    title: 'Capital',
+                                    // description: country.capital.toLocaleString()
+                                }
+                            ]
+                        }
+
+                        return <Card key={country.name} {...countryInfo} />
+                })}
+            </List>
         </Main>
     </>
   );
